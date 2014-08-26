8fc25f8d5d8c616a913a83d3a74b5c35288db7fa
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-26 10:24:58 -0700

    link to add a new animal

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   match('animals', {via: :get, to: 'animals#index'})
+  match('animals', {via: :post, to: 'animals#create'})
   match('animals/new', {via: :get, to: 'animals#new'})
   match('animals/:id', {via: :get, to: 'animals#show'})
 end
