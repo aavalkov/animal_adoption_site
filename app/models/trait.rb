9b244deececca11016924a806cd384e4634d2e6c
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-26 11:32:06 -0700

    validate presence of name for traits

---
+++ app/models/trait.rb
@@ -0,0 +1,3 @@
+class Trait < ActiveRecord::Base
+	validates :name, :presence => true
+end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -4,5 +4,4 @@ describe Animal do
 	it { should validate_presence_of :name }
 	it { should validate_presence_of :species }
 	it { should validate_presence_of :age }
-	it { should validate_presence_of :info }
 end
