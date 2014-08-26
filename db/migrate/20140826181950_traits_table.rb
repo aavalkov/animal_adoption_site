37dcc657cb9066d4af3e56646309198b9280f729
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-26 11:22:18 -0700

    add traits table

---
+++ db/migrate/20140826181950_traits_table.rb
@@ -0,0 +1,7 @@
+class TraitsTable < ActiveRecord::Migration
+  def change
+  	create_table :traits do |t|
+    	t.string :name
+  	end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826181702) do
+ActiveRecord::Schema.define(version: 20140826181950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140826181702) do
     t.string "name"
     t.string "age"
     t.string "species"
+  end
+
+  create_table "traits", force: true do |t|
+    t.string "name"
   end
 
 end
